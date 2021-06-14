12d8b72a32811420a7e3b62779ce45b1253cb51b
NVombat <nvombatkere@gmail.com>
2021-06-15 01:08:48 +0530

    pwn solve

--- Bandit/solve.py
+++ Bandit/solve.py
@@ -1,26 +1,28 @@
 import subprocess
+from pwn import *
 import json
 import glob
 
 config_data = json.load(open("b1.json"))
 #print(config_data)
 
-proc = subprocess.run(['sshpass', '-p', config_data['password'],
-'ssh', config_data['address'], '-p', config_data['port'], '-l', config_data['username']])
+#Use pwn to ssh into address and access password using commands
+s = ssh(host=config_data['address'], port=config_data['port'],
+user=config_data['username'], password=config_data['password'])
+sh = s.process('/bin/sh')
+pwd = s.process(config_data['commands']).recvall().decode()
+print(pwd)
 
-print(proc.returncode)
+#Save to file
+output_path = subprocess.getoutput("pwd")
+print("PATH:", output_path)
+files = glob.glob(output_path+"/passwords.txt")
 
-# output_pwd = subprocess.getoutput(config_data['commands'])
-# print(output_pwd)
-# output_path = subprocess.getoutput("pwd")
-# print("PATH:", output_path)
-# files = glob.glob(output_path+"/passwords.txt")
+if len(files)==0:
+    with open("passwords.txt", 'w') as pfile:
+        pfile.write("New Password New File\n")
+else:
+    with open("passwords.txt", 'a') as pfile:
+        pfile.write("New Password Old File\n")
 
-# if len(files)==0:
-#     with open("passwords.txt", 'w') as pfile:
-#         pfile.write("New Password New File\n")
-# else:
-#     with open("passwords.txt", 'a') as pfile:
-#         pfile.write("New Password Old File\n")
-
-# pfile.close()
+pfile.close()

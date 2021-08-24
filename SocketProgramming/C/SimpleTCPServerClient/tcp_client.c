abf33687046c7d3a6247e599ef454f687d9d41c3
NVombat <nvombatkere@gmail.com>
2021-08-24 23:10:50 +0530

    Simple TCP Server Client

--- SocketProgramming/C/SimpleTCPServerClient/tcp_client.c
+++ SocketProgramming/C/SimpleTCPServerClient/tcp_client.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-#define PORT 8000
+#define PORT 8050
 
 int main()
 {
-    int client_sock;
-    client_sock = socket(AF_INET, SOCK_STREAM, 0);
+    int sockfd, conn;
+    //Socket descriptor - Create a socket with a particular family, type and protocol 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
+    //The SOCKADDR_IN structure specifies a transport address and port for the AF_INET address family.
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); //An IN_ADDR structure that contains an IPv4 transport address.
 
-
-    connect(client_sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    //Connect to the server via the socket
+    connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
 
     printf("Client is connected...\n");
 
+    //Receive the message from the server and display it
     char serverresponse[1024];
-    recv(client_sock, &serverresponse, sizeof(serverresponse), 0);
+    recv(sockfd, &serverresponse, sizeof(serverresponse), 0);
     printf("Server message: %s\n", serverresponse);
 
-    close(client_sock);
+    //Close Socket
+    close(sockfd);
     printf("Client is closed...\n");
+
     return 0;
 }
+
+//To run
+// gcc -o client tcp_client.c
+// ./client
--- SocketProgramming/C/SimpleTCPServerClient/tcp_server.c
+++ SocketProgramming/C/SimpleTCPServerClient/tcp_server.c
@@ -5,35 +5,47 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-#define PORT 8000
+#define PORT 8050
 
 int main()
 {
-    int server_sock, conn;
+    int sockfd, conn;
     char servermessage[1024] = "Hi this is the server!";
 
-    server_sock = socket(AF_INET, SOCK_STREAM, 0);
+    //Socket descriptor - Create a socket with a particular family, type and protocol 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    //The SOCKADDR_IN structure specifies a transport address and port for the AF_INET address family.
+    struct sockaddr_in server_addr, client_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); //An IN_ADDR structure that contains an IPv4 transport address.
 
-    bind(server_sock, (struct sockaddr *) &addr, sizeof(addr));
-    listen(server_sock, 5);
+    //Bind the socket descriptor to the server address using the SOCKADDR_IN structure
+    bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+    //Listen for connections to the socket descriptor
+    listen(sockfd, 5);
 
     printf("Server is running...\n");
 
-    conn = accept(server_sock, NULL, NULL);
+    //Accept a connection from the client socket if and when data is sent
+    conn = accept(sockfd, (struct sockaddr *)NULL, NULL);
 
     printf("Server is connected...\n");
 
+    //Send data across the socket
     send(conn, servermessage, sizeof(servermessage), 0); 
 
     printf("Server message sent...\n");      
     
-    close(server_sock);
+    //Close Socket
+    close(sockfd);
     printf("Server closed...\n");
 
     return 0;
 }
+
+//To run
+// gcc -o server tcp_server.c
+// ./server

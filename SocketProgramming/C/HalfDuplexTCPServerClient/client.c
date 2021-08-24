726c452585ea4c0cd09bf73d62eda4c69098996e
NVombat <nvombatkere@gmail.com>
2021-08-25 03:15:00 +0530

    Half Duplex Chat init

---
+++ SocketProgramming/C/HalfDuplexTCPServerClient/client.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define PORT 8000
+
+int main()
+{
+
+
+    return 0;
+}
